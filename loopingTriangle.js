e31416f1d3ea2b443b8073fbc997acf57cd2784a
jm macapagal <jmdmacapagal@gmail.com>
2019-05-11 17:51:32 +0800

    added loopingTriangle code challenge

---
+++ loopingTriangle.js
@@ -0,0 +1,17 @@
+/* Looping a triangle
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+####### */
+
+let star = ''
+
+for (let i = 0; i < 7; i++) {
+    star+= '*'
+    console.log(star)
+}
